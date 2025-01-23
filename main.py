4d7d46f99b0c2aca84a7512ee97489bc1606334a
aviralsoni22 <aviralsoni22@gmail.com>
2025-01-23 13:26:53 +0530

    Data Transformation Completed

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 from src.ConciseAI.logging import logger
 from src.ConciseAI.pipeline.stage_1_data_ingestion_pipeline import DataIngestionTrainingPipeline
+from src.ConciseAI.pipeline.stage_2_data_transformtion_pipeline import DataTransformationTrainingPipeline
+
 STAGE_NAME = "Data Ingestion stage"
 
 try:
     logger.info(f"stage{STAGE_NAME} Initiated")
     data_ingestion_pipeline = DataIngestionTrainingPipeline()
     data_ingestion_pipeline.initiate_data_ingestion()
+    logger.info(f"Stage {STAGE_NAME} Completed")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Data Transformation Stage"
+
+try:
+    logger.info(f"stage{STAGE_NAME} Initiated")
+    data_transformation_pipeline = DataTransformationTrainingPipeline()
+    data_transformation_pipeline.initiate_data_transformation()
     logger.info(f"Stage {STAGE_NAME} Completed")
 except Exception as e:
     logger.exception(e)
