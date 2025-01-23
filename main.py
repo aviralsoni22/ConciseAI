5414c4668e418348e1cd6211a5cf5ab67fb49616
aviralsoni22 <aviralsoni22@gmail.com>
2025-01-23 14:53:02 +0530

    Evaluation Research Done

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from src.ConciseAI.logging import logger
 from src.ConciseAI.pipeline.stage_1_data_ingestion_pipeline import DataIngestionTrainingPipeline
 from src.ConciseAI.pipeline.stage_2_data_transformtion_pipeline import DataTransformationTrainingPipeline
+from src.ConciseAI.pipeline.stage_3_model_trainer_pipeline import ModelTrainerTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 
@@ -19,6 +20,18 @@ try:
     logger.info(f"stage{STAGE_NAME} Initiated")
     data_transformation_pipeline = DataTransformationTrainingPipeline()
     data_transformation_pipeline.initiate_data_transformation()
+    logger.info(f"Stage {STAGE_NAME} Completed")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Model Training Stage"
+
+try:
+    logger.info(f"stage{STAGE_NAME} Initiated")
+    modeltraining_pipeline = ModelTrainerTrainingPipeline()
+    modeltraining_pipeline.initiate_model_trainer()
     logger.info(f"Stage {STAGE_NAME} Completed")
 except Exception as e:
     logger.exception(e)
