75633e6dd85c25fb1cc3647bacd603fbe07a8235
aviralsoni22 <aviralsoni22@gmail.com>
2025-01-23 11:44:20 +0530

    1_data_ingestion completed

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
 from src.ConciseAI.logging import logger
-logger.info("Logging is implemented")
+from src.ConciseAI.pipeline.stage_1_data_ingestion_pipeline import DataIngestionTrainingPipeline
+STAGE_NAME = "Data Ingestion stage"
+
+try:
+    logger.info(f"stage{STAGE_NAME} Initiated")
+    data_ingestion_pipeline = DataIngestionTrainingPipeline()
+    data_ingestion_pipeline.initiate_data_ingestion()
+    logger.info(f"Stage {STAGE_NAME} Completed")
+except Exception as e:
+    logger.exception(e)
+    raise e
